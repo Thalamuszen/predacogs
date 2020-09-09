3cbf20aec2aa7fe82b6060448e3fbc1d8efd187c
Thalamuszen <thalamuszen@hotmail.com>
2020-09-09 20:24:52 +0100

    Update core.py

--- randimages/core.py
+++ randimages/core.py
@@ -24,14 +24,12 @@ _ = Translator("Image", __file__)
 @cog_i18n(_)
 class Core(commands.Cog):
 
+    __author__ = "Predä"
+    __version__ = "1.1.9"
+
     async def red_delete_data_for_user(self, **kwargs):
         """Nothing to delete."""
         return
-    
-        def format_help_for_context(self, ctx: commands.Context) -> str:
-        """Thanks Sinbad!"""
-        pre_processed = super().format_help_for_context(ctx)
-        return f"{pre_processed}\n\nAuthor: {self.__author__}\nCog Version: {self.__version__}"
 
     def __init__(self, bot: Red):
         self.bot = bot
@@ -39,6 +37,11 @@ class Core(commands.Cog):
 
     def cog_unload(self):
         self.bot.loop.create_task(self.session.close())
+
+    def format_help_for_context(self, ctx: commands.Context) -> str:
+        """Thanks Sinbad!"""
+        pre_processed = super().format_help_for_context(ctx)
+        return f"{pre_processed}\n\nAuthor: {self.__author__}\nCog Version: {self.__version__}"
 
     async def _get_reddit_imgs_simple(self, ctx: commands.Context, sub: list):
         """Get images from Reddit API."""
