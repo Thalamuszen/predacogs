36044a7cc28167809c9da8aa105ead93a06a54f2
Thalamuszen <thalamuszen@hotmail.com>
2020-09-09 20:31:26 +0100

    Update core.py

--- randimages/core.py
+++ randimages/core.py
@@ -32,11 +32,6 @@ class Core(commands.Cog):
     def cog_unload(self):
         self.bot.loop.create_task(self.session.close())
 
-    def format_help_for_context(self, ctx: commands.Context) -> str:
-        """Thanks Sinbad!"""
-        pre_processed = super().format_help_for_context(ctx)
-        return f"{pre_processed}\n\nAuthor: {self.__author__}\nCog Version: {self.__version__}"
-
     async def _get_reddit_imgs_simple(self, ctx: commands.Context, sub: list):
         """Get images from Reddit API."""
         try:
